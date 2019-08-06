e8c118404c576caf3b8d854f5d7483fa3f0d0f8d
Cameroncbrownjr <cameroncbrownjr@gmail.com>
2019-08-06 13:06:15 -0400

    mostly working

--- inqMysql/index.js
+++ inqMysql/index.js
@@ -6,20 +6,21 @@ let con = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'root',
+    password: 'Pumpkin1!',
     database: 'basic_products_DB'
 });
 //connect to the db
-con.connect(function(err){
+con.connect(function (err) {
     if (err) throw err;
+    displayAllProducts();
 });
 
-function displayAllProducts(cb) {
-    con.query('SELECT * FROM products', function(err, results) {
+function displayAllProducts() {
+    con.query('SELECT * FROM products', function (err, results) {
         console.log('\n')
         console.table(results)
         console.log('\n')
-        cb()
+        askToBuy()
     })
 }
 
@@ -30,17 +31,67 @@ function askToBuy() {
         message: 'Buy something or get the hell out!',
         choices: ['yes', 'no']
     })
-    .then(function(results) {
-        if (results.showAll === 'yes') {
-            clear()
-            console.log("Sorry we're actually closed!")
-            con.end()
-        } else {
-            clear()
-            console.log('bye!')
-            con.end()
-        }
-    })
+        .then(function (results) {
+            if (results.showAll === 'yes') {
+                // clear()
+                console.log("Watchu want?")
+                buyProducts()
+                // con.end()
+            } else {
+                clear()
+                console.log('bye!')
+                con.end()
+            }
+        })
 }
+function buyProducts() {
+    console.log("This is working");
+    // query the database for all items being auctioned
+    con.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        // once you have the items, prompt the user for which they'd like to bid on
+        inq
+            .prompt([
+                {
+                    name: "name",
+                    type: "rawlist",
+                    choices: function () {
+                        var productsArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            productsArray.push(results[i].name);
+                        }
+                        return productsArray;
+                    },
+                    message: "What item would you like to buy?"
+                },
+                {
+                    name: "howMany",
+                    type: "input",
+                    message: "How many would you like to buy?"
+                }
+            ])
+            .then(function (answer) {
+                // get the information of the chosen item
+                var chosenProduct;
+                console.log(answer.name)
 
-displayAllProducts(askToBuy)
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].name === answer.name) {
+                        chosenProduct = results[i].name;
+                        if (results[i].quantity >= answer.howMany){
+                            console.log("we have dis");
+                            results[i].quantity- answer.howMany;
+                        }
+                        else {
+                            console.log("we out")
+                        }
+                        
+                    }
+                }
+                console.log(chosenProduct);
+                console.log(answer.howMany);
+            })
+    }
+    )
+}
+// displayAllProducts(askToBuy)
--- inqMysql/schema.sql
+++ inqMysql/schema.sql
@@ -6,16 +6,11 @@ USE basic_products_DB;
 
 CREATE TABLE products
 (
-    id INTEGER(7) NOT NULL
-    AUTO_INCREMENT,
-    name VARCHAR
-    (60) NOT NULL,
-    price DECIMAL
-    (10,2) UNSIGNED NOT NULL,
-    quantity INTEGER
-    (7) NOT NULL,
-    PRIMARY KEY
-    (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR (60) NOT NULL,
+    price DECIMAL (10,2) NOT NULL,
+    quantity INTEGER (7) NOT NULL,
+    PRIMARY KEY (id)
 );
 
     INSERT INTO products
@@ -24,10 +19,10 @@ CREATE TABLE products
         ("Empty liquor bottle", 0.05, 100),
         ("Slightly used underwear", 5.00, 7),
         ("Cat poop nuggz", 0.05, 10000),
-        ("Wu-tang Sword", 2000000, 1),
-        ("Bubbles Deeecent Shopping carts", 50, 30),
+        ("Wu-tang Sword", 2000000.00, 1),
+        ("Bubbles Deeecent Shopping carts", 50.00, 30),
         ("Alfredo's Pizza", 4.21, 15),
-        ("Pizza by Alfredo", 10, 5),
-        ("Charlie's Rat Smasher", 1000, 1),
-        ("Fat Cats", 100000000, 2),
-        ("Motivation for this project", 420, 0);
+        ("Pizza by Alfredo", 15.00, 5),
+        ("Charlie's Rat Smasher", 1000.00, 1),
+        ("Fat Cats", 1000000.00, 2),
+        ("Motivation for this project", 420.00, 0);
